dcf972bd76d4430135030b210f07013d54443304
marelom <omaeyassinfannounpiano@gmail.com>
2026-09-12 18:49:11 +0200

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        My_Dict = {}
+
+        for s in strs:
+            key = ''.join(sorted(s))
+            if key in My_Dict:
+                My_Dict[key].append(s)
+            else:
+                My_Dict[key] = [s]    
+
+
+
+        return list(My_Dict.values())         
